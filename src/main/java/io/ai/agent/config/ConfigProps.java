8c6f02d731e98fcc6eabaff3b76eb627a00e5f66
Srinivasa Vasu <srinivasan.surprise@gmail.com>
2025-10-01 13:59:43 +0530

    [update] resource scan

--- src/main/java/io/ai/agent/config/ConfigProps.java
+++ src/main/java/io/ai/agent/config/ConfigProps.java
@@ -1,6 +1,5 @@
 package io.ai.agent.config;
 
-import io.micrometer.core.instrument.util.IOUtils;
 import jakarta.annotation.PostConstruct;
 import java.io.IOException;
 import java.nio.charset.Charset;
@@ -9,7 +8,7 @@ import lombok.Data;
 import lombok.Setter;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 import org.springframework.util.Assert;
 
@@ -26,32 +25,6 @@ public class ConfigProps {
 	private int acceptableDelayMinutes;
 	private Vector vector;
 	private Prompt prompt;
-
-	@PostConstruct
-	void loadPrompts() throws IOException {
-		String[] resources = prompt.getResources();
-		for (String resource : resources) {
-			String content = IOUtils.toString(new ClassPathResource(resource).getInputStream(), Charset.defaultCharset());
-			if (resource.equals(prompt.getAnalystPath())) {
-				prompt.setAnalystPrompt(content);
-			}
-			else if (resource.equals(prompt.getWeatherPath())) {
-				prompt.setWeatherPrompt(content);
-			}
-			else if (resource.equals(prompt.leadAnalystPath)) {
-				prompt.setLeadAnalystPrompt(content);
-			}
-			else if (resource.equals(prompt.getCritiquePath())) {
-				prompt.setCritiquePrompt(content);
-			}
-			else if (resource.equals(prompt.rankedKPath)) {
-				prompt.setRankedKPrompt(content);
-			}
-			else {
-				// do nothing
-			}
-		}
-	}
 
 	@PostConstruct
 	void validate() {
@@ -83,14 +56,23 @@ public class ConfigProps {
 		private String weatherPrompt;
 		@Setter(AccessLevel.PRIVATE)
 		private String rankedKPrompt;
-		private String analystPath;
-		private String leadAnalystPath;
-		private String critiquePath;
-		private String weatherPath;
-		private String rankedKPath;
+		private final Resource analystPath;
+		private final Resource leadAnalystPath;
+		private final Resource critiquePath;
+		private final Resource weatherPath;
+		private final Resource rankedKPath;
 
-		private String[] getResources() {
-			return new String[] {analystPath, critiquePath, leadAnalystPath, weatherPath, rankedKPath};
+		Prompt(Resource analystPath, Resource leadAnalystPath, Resource critiquePath, Resource weatherPath, Resource rankedKPath) throws IOException {
+			this.analystPath = analystPath;
+			this.leadAnalystPath = leadAnalystPath;
+			this.critiquePath = critiquePath;
+			this.weatherPath = weatherPath;
+			this.rankedKPath = rankedKPath;
+			setAnalystPrompt(analystPath.getContentAsString(Charset.defaultCharset()));
+			setCritiquePrompt(critiquePath.getContentAsString(Charset.defaultCharset()));
+			setLeadAnalystPrompt(leadAnalystPath.getContentAsString(Charset.defaultCharset()));
+			setWeatherPrompt(weatherPath.getContentAsString(Charset.defaultCharset()));
+			setRankedKPrompt(rankedKPath.getContentAsString(Charset.defaultCharset()));
 		}
 	}
 
